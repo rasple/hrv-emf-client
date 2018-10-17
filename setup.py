98882e77796106d0ed472b02aa548b05542d8f44
rasple <frankme97@gmail.com>
2018-10-17 09:20:27 +0200

    Changes to setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='hrv-emf-client',
+      version='0.1',
+      description='Client for HRV measurements',
+      url='https://github.com/rasple/hrv-emf-client',
+      author='Frank Meier',
+      author_email='it16078@lehre.dhbw-stuttgart.de',
+      license='GPLv3',
+      packages=find_packages(),
+      python_requires=">=3.3",
+      install_requires=[]
+      )
